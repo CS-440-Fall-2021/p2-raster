4323b98e617b2e09779d391589744be487eb8805
Anand Kumar <ak05173@st.habib.edu.pk>
2021-12-20 02:00:42 +0500

    Removed unneeded models

--- raytracer/build/buildPly.cpp
+++ raytracer/build/buildPly.cpp
@@ -18,7 +18,6 @@ void World::build(void)
   vplane.bottom_right = Point3D(1, -1, 2);
   vplane.hres = 200;
   vplane.vres = 200;
-  // acceleration_ptr = new KDTree(this);
 
   RGBColor grey(0.25);
 
@@ -40,23 +39,42 @@ void World::build(void)
   // ambient_ptr->scale_radiance(0.5);
   // set_ambient_light(ambient_ptr);
 
-  Point *light_ptr = new Point;
+  Point *light_ptr = new Point(RGBColor(0, 1, 0));
   light_ptr->set_shadows(true);
-  light_ptr->set_position(1, 1, 1);
+  light_ptr->set_position(1, -1, 1);
   light_ptr->scale_radiance(3.0);
   add_light(light_ptr);
+
+  Point *light_ptr1 = new Point(RGBColor(1, 1, 0));
+  light_ptr1->set_shadows(true);
+  light_ptr1->set_position(-1, 10, 0);
+  light_ptr1->scale_radiance(3.0);
+  add_light(light_ptr1);
+
+  Point *light_ptr2 = new Point(RGBColor(1, 153.0 / 255.0, 0));
+  light_ptr2->set_shadows(true);
+  light_ptr2->set_position(0, 50, 1);
+  light_ptr2->scale_radiance(3.0);
+  add_light(light_ptr2);
+
+  Point *light_ptr3 = new Point(RGBColor(204.0 / 255.0, 0, 204.0 / 255.0));
+  light_ptr3->set_shadows(true);
+  light_ptr3->set_position(0.5, 100, 1);
+  light_ptr3->scale_radiance(3.0);
+  add_light(light_ptr3);
+
+  Point *light_ptr4 = new Point(RGBColor(0, 0, 1));
+  light_ptr4->set_shadows(true);
+  light_ptr4->set_position(1, 100, 1);
+  light_ptr4->scale_radiance(3.0);
+  add_light(light_ptr4);
 
   Matte *pointerMT = new Matte;
   pointerMT->set_ka(0.2);
   pointerMT->set_kd(3);
   pointerMT->set_cd(RGBColor(0, 0, 1));
 
-  // Plane *plane_ptr = new Plane(Point3D(0, -1, 0), Vector3D(10, 5, 0));
-  // plane_ptr->set_material(pointerMT);
-
-  // add_geometry(plane_ptr);
-
   // filename goes here
-  add_ply("models/charmander.ply", pointerPH, Point3D(1, -1, -1),
-          Point3D(-1, 1, -3), true);
+  add_ply("models/combined.ply", pointerPH, Point3D(5, -1, -3),
+          Point3D(-5, 1, -1), true);
 }
--- raytracer/utilities/ChessPiece.cpp
+++
@@ -1,5 +0,0 @@
-#include "ChessPiece.hpp"
-#include <string>
-
-ChessPiece::ChessPiece(std::string name, double height, double diameter)
-    : name{name}, height{height}, diameter{diameter} {}
